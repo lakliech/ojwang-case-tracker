bbb4f204ff44add3e8300662d0a19b6b1f4716c7
lakliech <34573935+lakliech@users.noreply.github.com>
2025-06-15 02:40:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const path = require('path');
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static('public'));
 app.use(cookieParser());
@@ -27,8 +28,9 @@ const Conflict = mongoose.model('Conflict', new mongoose.Schema({
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, {
-  //useNewUrlParser: true, useUnifiedTopology: true
-});
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('✅ Connected to MongoDB'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
 // Auth Middleware
 function isAuthenticated(req, res, next) {
